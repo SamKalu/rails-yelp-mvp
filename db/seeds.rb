f1405ee2e669de201d7af56a3d4f895d78643396
Samuel <samueltitterington@gmail.com>
2021-08-25 03:28:45 +0200

    Feat: models, views, etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+7.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Games::Zelda.location,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
